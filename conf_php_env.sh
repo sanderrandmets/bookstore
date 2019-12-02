1881e2c349927e53e33adaf52f0e30ff546151a5
Sander <sander.randmets@ametikool.ee>
2019-12-02 14:17:49 +0200

    initial commit

---
+++ conf_php_env.sh
@@ -0,0 +1,13 @@
+#1/bin/bash
+
+echo "hello, world!"
+export DEBIAN_FRONTEND=noninteractive
+apt update -y
+apt upgrade -y
+service mysql start
+mysql -u root <<EOF
+UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE user = 'root' AND plugin = 'unix_socket';
+FLUSH PRIVILEGES;
+CREATE DATABASE Books;
+EOF
+mysql -u root Books < Books.sql
