1cc3cdfc7b449e342850987f8697350b827a58f7
Sander <sander91112@gmail.com>
2019-12-09 15:55:52 +0200

    title and year filter search

--- index.php
+++ index.php
@@ -15,13 +15,45 @@ try {
 } catch (\PDOException $e) {
      throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
+
+//var_dump($_GET);
+
+$title = $_GET['title'];
+$year = $_GET['year'];
+$stmt = $pdo->prepare('SELECT * FROM books WHERE title LIKE :title AND release_date LIKE :year');
+$stmt->execute(['title' => '%' . $title . '%', 'year' => $year]);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <title>Document</title>
+</head>
+<body>
+     <h1>Otsing</h1>
+
+     <form action="index.php" method="get">
+          Pealkiri:
+          <br>
+          <input type="text" name="title" placeholder="Pealkiri" value="<?=$title?>" style="margin: 4px">
+          <br>
+          <input type="text" name="year" placeholder="Aasta" value="<?=$year?>" style="margin: 4px">
+          <br>
+          <input type="submit" value="Otsi" style="margin: 4px">
+     </form>
+     <ul>
+
 <?php
-
-$stmt = $pdo->prepare('SELECT * FROM Books WHERE release_date LIKE :year');
-$stmt->execute(['year' => 2000]);
-
 while ( $row =$stmt->fetch() ){
 
-    
+    echo '<li>' . $row['title'] . '</li>';
 }
+?>
+
+     </ul> 
+</body>
+</html>
+
