e14aade63fcc4b876c280c0b944225b6816c931c
Sander <sander91112@gmail.com>
2020-01-14 11:22:15 +0200

    make a link

--- book.php
+++ book.php
@@ -27,7 +27,7 @@ Hind: <?php echo $book['price'];?><br>
 Lao seis: <?php echo $book['stock_saldo'];?><br>
 Lehekülgede arv <?php echo $book['pages'];?><br>
 Tüüp: <?php echo $book['type'];?><br>
-<a href="Delete">
+<a href="delete.php?id=<?php echo $id?>">DELETE</a>
 </body>
 </html>
 
